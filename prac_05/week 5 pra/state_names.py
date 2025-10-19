58fa7d4dfa36ccba2cdef9a6020e1d45a9504f18
DELL <hengyi.zhou@my.jcu.edu.au>
2025-10-19 13:15:33 +0800

    # Convert to uppercase for dictionary lookup

---
+++ prac_05/week 5 pra/state_names.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+
+def main():
+    print(CODE_TO_NAME)
+    state_code = input("Enter short state: ").strip()
+
+    while state_code != "":
+        try:
+            # Convert to uppercase for dictionary lookup
+            print(f"{state_code.upper()} is {CODE_TO_NAME[state_code.upper()]}")
+        except KeyError:
+            print("Invalid short state")
+
+        state_code = input("Enter short state: ").strip()
+
+    # Print all states neatly when finished
+    print("\nAll states and names:")
+    key_width = max(len(k) for k in CODE_TO_NAME)
+    for code, name in sorted(CODE_TO_NAME.items()):
+        print(f"{code:{key_width}} is {name}")
+
+main()
+
+
