70ca09a845040b8cd62e2a95405b3819bfcbc4fb
DELL <hengyi.zhou@my.jcu.edu.au>
2025-11-09 11:40:41 +0800

    Write guitars to a CSV file.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -16,3 +16,11 @@ def load_guitars(filename):
             guitars.append(Guitar(name, year, cost))
     in_file.close()
     return guitars
+
+def save_guitars(filename, guitars):
+    """Write guitars to a CSV file."""
+    out_file = open(filename, "w", encoding="utf-8", newline="")
+    writer = csv.writer(out_file)
+    for guitar in guitars:
+        writer.writerow([guitar.name, guitar.year, f"{guitar.cost:.2f}"])
+    out_file.close()
